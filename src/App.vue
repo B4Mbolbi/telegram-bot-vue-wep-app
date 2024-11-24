67a5d4167da84995ba65b1ddecf176c12565dea6
B4MBOLBI <tytor5674onli@gmail.com>
2024-11-25 00:18:54 +1000

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,16 @@
 <script>
-export default {
-  data() {
-    return {
-      name: TWA.initDataUnsafe.user.first_name,
-      username: TWA.initDataUnsafe.user.username,
-      id: TWA.initDataUnsafe.user.id,
-      language: TWA.initDataUnsafe.user.language_code,
-      photo: TWA.initDataUnsafe.user.photo,
-      platform: TWA.platform
-    }
-  }
-
-}
 
 </script>
 
 <template>
   <div class="container">
     <h1>BotB4mbolbi</h1>
-    <h2>Platform: {{ platform }}</h2>
-    <h2>Name: {{ name }}</h2>
-    <h2>Username: {{ username }}</h2>
-    <h2>ID: {{ id }}</h2>
-    <h2>Language: {{ language }}</h2>
-    <h2>Photo: {{ photo }}</h2>
+    <h2>Platform: {{ TWA.platform }}</h2>
+    <h2>Name: {{ TWA.initDataUnsafe.user.first_name}}</h2>
+    <h2>Username: {{ TWA.initDataUnsafe.user.username }}</h2>
+    <h2>ID: {{ TWA.initDataUnsafe.user.id }}</h2>
+    <h2>Language: {{ TWA.initDataUnsafe.user.language_code }}</h2>
+    <h2>Photo: {{TWA.initDataUnsafe.user.photo }}</h2>
   </div>
 
 </template>
