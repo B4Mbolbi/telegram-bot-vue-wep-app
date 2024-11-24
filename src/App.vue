e34faefd91af243e7ce56491e1d2ec146c286d46
B4MBOLBI <tytor5674onli@gmail.com>
2024-11-25 00:24:23 +1000

    init commit 1

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,11 @@
   <div class="container">
     <h1>BotB4mbolbi</h1>
     <h2>Platform: {{ TWA.platform }}</h2>
-    <h2>Name: {{ TWA.initDataUnsafe.user}}</h2>
+    <h2>Name: {{ TWA.initDataUnsafe.user.first_name}}</h2>
     <h2>Username: {{ TWA.initDataUnsafe.user.username }}</h2>
     <h2>ID: {{ TWA.initDataUnsafe.user.id }}</h2>
     <h2>Language: {{ TWA.initDataUnsafe.user.language_code }}</h2>
-    <h2>Photo: {{TWA.initDataUnsafe.user.photo }}</h2>
+
   </div>
 
 </template>
